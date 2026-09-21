47b745c0caeb197895c39b7430cf22bd1b5da842
Boateng Prince Agyenim <isbbydior@gmail.com>
2026-09-21 22:37:47 +0000

    refactor (codebase): Replace monolithic architecture with modularized codebase

---
+++ src/js/PERF.js
@@ -0,0 +1,10 @@
+// PERF.js
+// ============================================================================
+// Performance monitoring dummy-methods.
+// ============================================================================
+
+// Dummy no-op functions.
+// I use these in a special build for custom performance profiling.
+const PERF_START = () => { };
+const PERF_END = () => { };
+const PERF_UPDATE = () => { };
